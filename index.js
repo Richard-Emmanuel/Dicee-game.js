f1ff9eb9c9e8c3b25c960c86ab2f93a3ca7e4b9c
Richard-Emmanuel <richardnuelofficial@gmail.com>
2022-07-12 02:23:59 +0100

    modified js file

--- index.js
+++ index.js
@@ -17,3 +17,11 @@ var randomNumber2 = Math.floor(Math.random() * 6)+1;
 var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2 );
+
+if (randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = "🚩Player 1 Wins"
+}else if(randomNumber1 == randomNumber2){
+  document.querySelector("h1").innerHTML = "Draw!"
+}else{
+  document.querySelector("h1").innerHTML = "Player 2 Wins 🚩"
+}
